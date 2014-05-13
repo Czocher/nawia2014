2852cb7e3380b148def731e14f47b064034fe27c
MCin22 <mcin22@gmail.com>
2014-05-13 20:18:39 +0200

    Drobne poprawki w urlach
    Change-Id: I34358214248eb607485b62c198b44a99a0a4e0a7

--- Nawia2014/Nawia2014/student/urls.py
+++ Nawia2014/Nawia2014/student/urls.py
@@ -3,17 +3,17 @@ from student.views import List
 from student.views import ListFiltered
 from student.views import Observed
 
-urlpatterns = patterns('',
-    (r'^$', 'student.views.home'),
-    (r'^home$', 'student.views.home'),
-    (r'^help$', 'student.views.help'),
-    (r'^account$', 'student.views.account'),
+urlpatterns = patterns('student.views',
+    (r'^$', 'home'),
+    (r'^home$', 'home'),
+    (r'^help$', 'help'),
+    (r'^account$', 'account'),
 
     (r'^list$', List.as_view()),
     (r'^list/([\w-]+)/$', ListFiltered.as_view()),
-    (r'^list/preview/(?P<subject_id>[0-9]+)$', 'student.views.preview_list'),
-    (r'^list/addToObserved/(?P<id>[0-9]+)$', 'student.views.addToObserved_list'),
-    (r'^list/addToQueue/(?P<id>[0-9]+)$', 'student.views.addToQueue_list'),
+    (r'^list/preview/(?P<subject_id>[0-9]+)$', 'preview_list'),
+    (r'^list/addToObserved/(?P<id>[0-9]+)$', 'addToObserved_list'),
+    (r'^list/addToQueue/(?P<id>[0-9]+)$', 'addToQueue_list'),
 
     (r'^supervisor/contact/(?P<id>[0-9]+)$', List.as_view()),
 
@@ -35,7 +35,4 @@ urlpatterns = patterns('',
     (r'^asigned/abandon/(?P<id>[0-9]+)$', List.as_view()),
     (r'^asigned/sendFiles/(?P<id>[0-9]+)$', List.as_view()),
     (r'^asigned/contact/(?P<id>[0-9]+)$', List.as_view()),
-    
-    #(r'^articles/(\d{4})/(\d{2})/$', 'news.views.month_archive'),
-    #(r'^articles/(\d{4})/(\d{2})/(\d+)/$', 'news.views.article_detail'),
 )
