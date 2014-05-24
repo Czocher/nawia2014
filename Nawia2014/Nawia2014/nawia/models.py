2a8d279f5779d29e97b0472b2e4a2389102e58dc
Damian Zalewski <damiano724@gmail.com>
2014-05-24 12:43:29 +0200

    Internacjonalizacja modeli. Dodanie polskiej wersji językowej.
    Change-Id: Ic1864ea12acfab2a0c4819b5cf86a498855ea43b

--- Nawia2014/Nawia2014/nawia/models.py
+++ Nawia2014/Nawia2014/nawia/models.py
@@ -3,42 +3,50 @@
 import pytz
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser 
 from datetime import datetime
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 class ThesisSubject(models.Model):
     u'''
     Temat pracy dyplomowej
     '''
-    statesHistory = models.ManyToManyField('ThesisSubjectStateChange', null = True, blank = True)
+    statesHistory = models.ManyToManyField('ThesisSubjectStateChange', null = True, blank = True,
+                                           verbose_name = _('history of states changes'))
     # self.state : ThesisSubjectStateChange
 
-    title = models.CharField(max_length = 511)
-    description = models.TextField()
+    title = models.CharField(max_length = 511, 
+                             verbose_name =_('thesis title'))
+    description = models.TextField(verbose_name=_('thesis description'))
     # słowa kluczowe związane z problematyką poruszaną w pracy
-    keywords = models.ManyToManyField('Keyword', null = True, blank = True)
+    keywords = models.ManyToManyField('Keyword', null = True, blank = True, 
+                                      verbose_name =_('keywords'))
     
     # TODO obsługa prac dedykowanych
     # isDedicated = models.BooleanField(default = false)
     # dedicationTargets = models.ManyToManyField('Student')
     
     # maksymalna liczba osób w zespole, jeśli jest to praca zespołowa
-    teamMembersLimit = models.PositiveSmallIntegerField(default = 1)
+    teamMembersLimit = models.PositiveSmallIntegerField(default = 1, 
+                                                        verbose_name =_('maximum number of team members'))
     # self.isTeamWork = teamMembersLimit > 1 ? True : False
 
     # autor tematu - co najmniej doktor lub firma zewnętrzna
     # relacja z modelem 'User', ponieważ autor pracy może być klasy 'Employee' lub 'Organization'
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, 
+                               verbose_name =_('author of thesis subject'))
 
-    attachments = models.ManyToManyField('Attachment', null = True, blank = True)
+    attachments = models.ManyToManyField('Attachment', null = True, blank = True, 
+                                         verbose_name =_('attachments to thesis'))
 
     # pytania, na które musi odpowiedzieć osoba zgłaszająca chęć pisania pracy na dany temat
     # na podstawie wartości kryteriów ustalana jest kolejność studentów na liście zainteresowanych
-    criteria = models.ManyToManyField('SubmissionCriterion', null = True, blank = True)
+    criteria = models.ManyToManyField('SubmissionCriterion', null = True, blank = True, 
+                                        verbose_name =_('questions for the student who apply to write a given thesis'))
 
     # flaga określająca czy praca ma zostać opublikowana automatycznie po jej zatwierdzeniu przez wyższe szczeble
-    isAutoPublished = models.BooleanField(default = True)
+    isAutoPublished = models.BooleanField(default = True, 
+                                            verbose_name = _('information about automatically published thesis after approval'))
 
     def __getattr__(self, name):
         if name == 'state':
@@ -52,8 +60,8 @@ class ThesisSubject(models.Model):
         return '"%s" - %s' % (self.title, self.author)
 
     class Meta:
-        verbose_name = 'temat pracy dyplomowej'
-        verbose_name_plural = 'tematy prac dyplomowych'
+        verbose_name = _('subject of thesis')
+        verbose_name_plural = _('subjects of theses')
 
 class ThesisSubjectStateChange(models.Model):
     u'''
@@ -79,15 +87,19 @@ class ThesisSubjectStateChange(models.Model):
     )
     state = models.CharField(max_length = 2,
                              choices = THESIS_SUBJECT_STATE_CHOICES,
-                             default = DRAFT)
+                             default = DRAFT,
+                             verbose_name = _('actual thesis subject state'))
 
     # osobami mogącymi powodować zmianę stanu są kierownicy katedr, dziekani/prodziekani lub autor pracy
     # relacja z modelem 'User', ponieważ autor pracy może być klasy 'Employee' lub 'Organization'
-    initiator = models.ForeignKey(User, null = True, blank = True)
+    initiator = models.ForeignKey(User, null = True, blank = True, 
+                                    verbose_name = _('person responsible for changing thesis subject state'))
     # dodatkowa informacja nt. zmiany stanu, uzasadnienie decyzji np. odrzucenia tematu
-    comment = models.TextField(blank = True)
+    comment = models.TextField(blank = True, 
+                               verbose_name = _('note about thesis subject state'))
 
-    occuredAt = models.DateTimeField(editable = False, null = True, blank = True)
+    occuredAt = models.DateTimeField(editable = False, null = True, blank = True, 
+                                     verbose_name = _('modification date'))
 
     def save(self, *args, **kwargs):
         if self.id == None:
@@ -99,6 +111,8 @@ class ThesisSubjectStateChange(models.Model):
 
     class Meta:
         get_latest_by = "occuredAt"
+        verbose_name = _('change subject of thesis state')
+        verbose_name_plural = _('changes subjects of theses states')
 
 class Thesis(models.Model):
     u'''
@@ -112,23 +126,30 @@ class Thesis(models.Model):
     UNKNOWN_REVIEWS_PROGRESS = 'unknown reviews progress'
 
     # zapis na pracę zawiera dane typu temat, autor tematu, autor pracy
-    authorship = models.OneToOneField('Authorship')
-    isDone = models.BooleanField(default = False)
-    attachments = models.ManyToManyField('Attachment', null = True, blank = True)
+    authorship = models.OneToOneField('Authorship', 
+                                      verbose_name = _('informations about thesis'))
+    isDone = models.BooleanField(default = False, 
+                                 verbose_name = _('inform if thesis is done'))
+    attachments = models.ManyToManyField('Attachment', null = True, blank = True, 
+                                         verbose_name = ('attachments'))
 
     # promotor
     supervisor = models.ForeignKey('Employee',
-                                   related_name = 'supervisedThesis')
+                                   related_name = 'supervisedThesis',
+                                   verbose_name = _('supervisor'))
     # promotorzy pomocniczy
     auxiliarySupervisors = models.ManyToManyField('Employee',
                                                   related_name = 'auxiliarySupervisedThesis',
-                                                  null = True, blank = True)
+                                                  null = True, blank = True,
+                                                  verbose_name = _('auxiliary supervisors'))
     # gdy autor tematu jest instutucją zewnętrzną, która nie ma prawa bycia promotorem
     advisor = models.ForeignKey('Organization',
                                 related_name = 'advisedThesis',
-                                null = True, blank = True)
+                                null = True, blank = True,
+                                verbose_name = _('university employee supervising thesis if author is external organization'))
 
-    reviews = models.ManyToManyField('Review', null = True, blank = True)
+    reviews = models.ManyToManyField('Review', null = True, blank = True,
+                                     verbose_name = _('thesis reviews'))
     
     # self.state
     # TODO zastanowić się nad stanami recenzji
@@ -172,7 +193,11 @@ class Thesis(models.Model):
 
     def __unicode__(self):
         return '"%s", %s, %s' % (self.subject, self.student, self.supervisor)
-
+    
+    class Meta:
+        verbose_name = _('thesis')
+        verbose_name_plural = _('theses')
+        
 class Authorship(models.Model):
     u'''
     Powiązanie studenta z tematem pracy, na który się początkowo zapisuje, a następnie (po zaakceptowaniu przez promotora) realizuje.
@@ -189,17 +214,23 @@ class Authorship(models.Model):
     )
     state = models.CharField(max_length = 1,
                              choices = AUTHORSHIP_STATE_CHOICES,
-                             default = PROPOSED)
+                             default = PROPOSED,
+                             verbose_name = _('authorship state'))
 
-    thesisSubject = models.ForeignKey('ThesisSubject')
+    thesisSubject = models.ForeignKey('ThesisSubject',
+                                      verbose_name = _('subject of thesis'))
     # dodatkowe informacje przekazywane autorowi tematu,
     # np. uzasadnienie chęci pisania pracy na dany temat lub przypomnienie o odbytej rozmowie
-    comment = models.TextField(blank = True)
+    comment = models.TextField(blank = True,
+                               verbose_name = _('informations from student to thesis subject author'))
     
-    student = models.ForeignKey('Student')
+    student = models.ForeignKey('Student',
+                                verbose_name = _('student'))
 
-    createdAt = models.DateTimeField(editable = False, null = True, blank = True)
-    updatedAt = models.DateTimeField(editable = False, null = True, blank = True)
+    createdAt = models.DateTimeField(editable = False, null = True, blank = True,
+                                     verbose_name = _('date of create authorship'))
+    updatedAt = models.DateTimeField(editable = False, null = True, blank = True,
+                                     verbose_name = _('date of last modification'))
 
     def save(self, *args, **kwargs):
         if self.id != None:
@@ -210,6 +241,10 @@ class Authorship(models.Model):
 
     def __unicode__(self):
         return '"%s", %s (%s)' % (self.subject, self.student, self.get_state_display())
+        
+    class Meta:
+        verbose_name = _('authorship')
+        verbose_name_plural = _('authorships')
 
 class SubmissionCriterion(models.Model):
     u'''
@@ -224,18 +259,26 @@ class SubmissionCriterion(models.Model):
         (FLOAT, 'float'),
     )
     type = models.CharField(max_length = 1,
-                            choices = CRITERION_TYPE_CHOICES)
-    label = models.CharField(max_length = 255)
+                            choices = CRITERION_TYPE_CHOICES,
+                            verbose_name = _('type of the criterion'))
+    label = models.CharField(max_length = 255,
+                             verbose_name = _('content of the criterion'))
 
     def __unicode__(self):
         return '%s (%s)' % (self.label, self.get_type_display())
-
+    
+    class Meta:
+        verbose_name = _('submission criterion')
+        verbose_name_plural = _('submission criteria')
+        
 class SubmissionCriterionValue(models.Model):
     u'''
     Para (kryterium, wartość/odpowiedź), np. (Ocena z programowania obiektowego (float), '3.5').
     '''
-    criterion = models.ForeignKey('SubmissionCriterion')
-    __value = models.CharField(max_length = 15, db_column = 'value')
+    criterion = models.ForeignKey('SubmissionCriterion',
+                                  verbose_name = _('criterion'))
+    __value = models.CharField(max_length = 15, db_column = 'value',
+                               verbose_name = _('value of criterion'))
 
     # self.value getter
     def __getValue(self):
@@ -269,7 +312,11 @@ class SubmissionCriterionValue(models.Model):
 
     def __unicode__(self):
         return '%s => %s' % (self.criterion, self.value)
-
+    
+    class Meta:
+        verbose_name = _('submission criterion value')
+        verbose_name_plural = _('submission criteria values')
+        
 class Review(models.Model):
     SUPERVISOR = 's'
     REVIEWER = 'r'
@@ -280,66 +327,97 @@ class Review(models.Model):
         (AUXILIARY_REVIEWER, 'auxiliary reviewer'),
     )
     authorType = models.CharField(max_length = 1,
-                             choices = REVIEW_AUTHOR_TYPE_CHOICES)
-    author = models.ForeignKey('Employee')
+                             choices = REVIEW_AUTHOR_TYPE_CHOICES,
+                             verbose_name = _('review author type'))
+    author = models.ForeignKey('Employee',
+                                verbose_name = _('review author name'))
 
-    comment = models.TextField(blank = True)
-    mark = models.FloatField(null = True, blank = True) # dodać walidację
-    isDone = models.BooleanField(default = False)
+    comment = models.TextField(blank = True,
+                               verbose_name = _("reviewer's comment"))
+    mark = models.FloatField(null = True, blank = True,
+                             verbose_name = _('mark')) # dodać walidację
+    isDone = models.BooleanField(default = False,
+                                 verbose_name = _('information about adding review'))
 
-    attachments = models.ManyToManyField('Attachment', null = True, blank = True)
+    attachments = models.ManyToManyField('Attachment', null = True, blank = True,
+                                         verbose_name = _('attachments to review'))
 
     def __unicode__(self):
         return '%s (%s) => %s' % (self.author, self.get_authorType_display(), self.mark)
-
+    
+    class Meta:
+        verbose_name = _('review')
+        verbose_name_plural = _('reviews')
+        
 class Attachment(models.Model):
-    pass
+    class Meta:
+        verbose_name = _('attachment')
+        verbose_name_plural = _('attachments')
 
 class Keyword(models.Model):
-    pass
+    class Meta:
+        verbose_name = _('keyword')
+        verbose_name_plural = _('keywords')
 
 class StudyCycle(models.Model):
     u'''
     Cykl kształcenia (np. informatyka, inżynierskie, stacjonarne, 2011Z-2014Z)
     '''
-    ldapId = models.CharField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
+    ldapId = models.CharField(max_length = 255, unique = True,
+                              verbose_name = _('LDAP ID'))
+    name = models.CharField(max_length = 255,
+                            verbose_name = _('study cycle name'))
 
-    submissionsOpenAt = models.DateField(default=None, null=True, blank=True)
-    submissionsCloseAt = models.DateField(default=None, null=True, blank=True)
-    isLdapSynced = models.BooleanField(default=False)
-
+    submissionsOpenAt = models.DateField(default=None, null=True, blank=True,
+                                         verbose_name = _('start study date'))
+    submissionsCloseAt = models.DateField(default=None, null=True, blank=True,
+                                          verbose_name = _('finish study date'))
+    isLdapSynced = models.BooleanField(default=False,
+                                       verbose_name = _('information thath LDAP is synced'))
+        
     def __unicode__(self):
         return self.name
-
-
+    
+    class Meta:
+        verbose_name = _('study cycle')
+        verbose_name_plural = _('study cycles')
 
 class UserBasedModel(models.Model):
-    user = models.OneToOneField(User)
-    isLdapSynced = models.BooleanField(default=False)
+    user = models.OneToOneField(User, verbose_name = _('user'))
+    isLdapSynced = models.BooleanField(default=False, 
+                                       verbose_name = _('information thath LDAP is synced'))
 
     class Meta:
         abstract = True
-
-
+        verbose_name = _('user based model')
+        verbose_name_plural = _('users based models')
+        
 class Student(UserBasedModel):
     # jeden student może realizować wiele cykli kształcenia
-    studyCycles = models.ManyToManyField('StudyCycle', null = True, blank = True) # tymczasowo
+    studyCycles = models.ManyToManyField('StudyCycle', null = True, blank = True,
+                                         verbose_name = _('study cycle')) # tymczasowo
 
     def __unicode__(self):
         return unicode(self.user)
+    
+    class Meta:
+        verbose_name = _('student')
+        verbose_name_plural = _('students')
 
 class Employee(UserBasedModel):
     u'''
     Pracownik uczelni
     '''
     # tytuł naukowy
-    title = models.CharField(max_length = 255, blank = True)
+    title = models.CharField(max_length = 255, blank = True,
+                             verbose_name = _('academic title'))
     # stanowisko pracy (asystent, adiunkt, itp.)
-    position = models.CharField(max_length = 255, blank = True)
+    position = models.CharField(max_length = 255, blank = True,
+                                verbose_name = _('worksite'))
 
     # jednostka organizacyjna (katedra, zakład, itp.), do której należy dany pracownik
-    organizationalUnit = models.ForeignKey('OrganizationalUnit')
+    organizationalUnit = models.ForeignKey('OrganizationalUnit',
+                                            verbose_name = _('organizational unit'))
 
     # self.canSupervise : bool
     # self.canReview : bool
@@ -354,26 +432,42 @@ class Employee(UserBasedModel):
 
     def __unicode__(self):
         return '%s %s' % (self.title, self.user)
+        
+    class Meta:
+        verbose_name = _('employee')
+        verbose_name_plural = _('employess')
 
 class Organization(UserBasedModel):
     u'''
     Organizacja zewnętrzna (firma, stowarzyszenie, urząd, itp.)
     '''
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length = 255,
+                            verbose_name = _('organization name'))
 
     def __unicode__(self):
         return '%s, %s' % (self.name, self.user)
+    
+    class Meta:
+        verbose_name = _('organization')
+        verbose_name_plural = _('organizations')
 
 class OrganizationalUnit(models.Model):
     u'''
     Jednostka organizacyjna (katedra, dziekanat, itp.)
     '''
-    ldapId = models.CharField(max_length = 31, unique = True)
-    name = models.CharField(max_length = 255)
-    head = models.ForeignKey(Employee, null = True, blank = True) # 'null = True' prawdopodonie tymczasowo
+    ldapId = models.CharField(max_length = 31, unique = True,
+                              verbose_name = _('LDAP ID'))
+    name = models.CharField(max_length = 255,
+                            verbose_name = _('organizational unit name'))
+    head = models.ForeignKey(Employee, null = True, blank = True,
+                             verbose_name = _('organizational unit head')) # 'null = True' prawdopodonie tymczasowo
 
     def __unicode__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = _('organizational unit')
+        verbose_name_plural = _('organizational units')
 
 class Authority(models.Model):
     u'''
@@ -390,9 +484,15 @@ class Authority(models.Model):
         (VICE_DEAN_FOR_STUDENTS, 'Vice-Dean for Teaching and Students'),
     )
     role = models.CharField(max_length = 2,
-                            choices = AUTHORITY_ROLES_CHOICES)
-    occupant = models.ForeignKey('Employee')
+                            choices = AUTHORITY_ROLES_CHOICES,
+                            verbose_name = _('authority role'))
+    occupant = models.ForeignKey('Employee',
+                                 verbose_name = _('person occupying the worksite'))
 
     def __unicode__(self):
         return '%s => %s' % (self.get_role_display(), self.occupant)
+    
+    class Meta:
+        verbose_name = _('authority')
+        verbose_name_plural = _('authorities')
 
