3b42814379307a0df18c632f78c16686f63b7e02
Marcin Żukowski <marcin@zukowski.at>
2014-05-25 22:56:19 +0200

    Dodałem szkielet modułu przydzielającego uprawnienia.
    Change-Id: I52c195922be72e04d0366eedd6d94c086d6a1d74

--- Nawia2014/Nawia2014/nawia/admin.py
+++ Nawia2014/Nawia2014/nawia/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.db.models import get_models, get_app
+from django.contrib.auth.models import Permission
+
+admin.site.register(Permission)
 
 for model in get_models(get_app('nawia')):
     admin.site.register(model)
+
