db334ba4628285a0e824e541c0ec5523e13cf4b0
Paweł Jan Czochański <czochanski@gmail.com>
2014-06-24 23:14:41 +0200

    Name fix.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Nawia2014.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nawia2014.settings")
 
     from django.core.management import execute_from_command_line
 
